e30f2dc6ff9fa2d0cd0602152163cd7788e3354b
ZvoneKralj <147518712+ZvoneKralj@users.noreply.github.com>
2025-03-18 17:06:21 +0100

    fix: riješen konflikt

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def main():
 
 if __name__ == "__main__":
     main()
-   
-   # PIS-Vjezba1
-def README.md():
+
+# PIS-Vjezba1
+def readme():
     print("Python projekt za vježbu korištenja Git sustava za verzioniranje.")
 
-if __name__ == "__README.md__":
-    README.md()
+if __name__ == "__main__":
+    readme()
+
 # Ovo je moj komentar -RenatoCarić
 def nova_metoda():
     return "Ovo je nova metoda 2."
